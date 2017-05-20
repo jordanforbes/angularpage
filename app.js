80e957c637c3c1ff538d534f74b17962abcebae4
Jordan Forbes <Jordforb@gmail.com>
2017-05-20 18:35:17 -0400

    new port method

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var connect = require('connect');
 var serveStatic = require('serve-static')
 
-connect().use(serveStatic(__dirname)).listen(5000, function(){
-	console.log('Server is running on 5000	')
-})
+// connect().use(serveStatic(__dirname)).listen(5000, function(){
+// 	console.log('Server is running on 5000	')
+// })
+
+
+connect().use(serveStatic(__dirname)).listen(process.env.PORT || 5000)
+
