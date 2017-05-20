42bdfc6120d95ce2cea56bf20cf572392309fa3e
Jordan Forbes <Jordforb@gmail.com>
2017-05-20 18:25:08 -0400

    removed dups

--- profilepage/server.js
+++
@@ -1,6 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static')
-
-connect().use(serveStatic(__dirname)).listen(8098, function(){
-	console.log('Server is running on 8098	')
-})
