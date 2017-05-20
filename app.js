f225e2b982cf2348844cac3557dbb2ae1af40c05
Jordan Forbes <Jordforb@gmail.com>
2017-05-20 18:31:08 -0400

    switched ports

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static')
 
-connect().use(serveStatic(__dirname)).listen(8098, function(){
-	console.log('Server is running on 8098	')
+connect().use(serveStatic(__dirname)).listen(5000, function(){
+	console.log('Server is running on 5000	')
 })
